33a5a8163bc96ac11a6f75d92c128edb92d180d0
adsstudenttw <tom.westerhout@student.hu.nl>
2026-02-08 18:11:08 +0100

    Created HSPOT config and data processing class

---
+++ data/hspot.py
@@ -0,0 +1,25 @@
+import os
+from data.dancetrack import DanceTrack
+
+
+class HSPOT(DanceTrack):
+    def __init__(
+        self,
+        data_root: str = "./datasets/",
+        sub_dir: str = "HSPOT",
+        split: str = "train",
+        load_annotation: bool = True,
+    ):
+        super().__init__(
+            data_root=data_root,
+            sub_dir=sub_dir,
+            split=split,
+            load_annotation=load_annotation,
+        )
+        return
+
+    @staticmethod
+    def _get_image_path(sequence_dir, frame_idx):
+        return os.path.join(
+            sequence_dir, "img1", f"{frame_idx + 1:06d}.jpg"
+        )  # different from DanceTrack
